5acb7f3aa65b8b42ba25170c863b20a80986347b
kejunxiao <shock_it@163.com>
2018-04-13 14:14:53 +0800

    eval into dev dataset, accuracy is 0.68-0.69

--- text_clf/data_load.py
+++ text_clf/data_load.py
@@ -14,8 +14,7 @@ import params
 class DataLoad(object):
     logging.getLogger().setLevel(logging.INFO)
 
-    def __init__(self, data_path, mode, batch_size, num_epochs, forced_seq_len=None,
-                 dev_sample_rate=0.2):
+    def __init__(self, data_path, batch_size, num_epochs, forced_seq_len=None, dev_sample_rate=0.05):
         """
         params:
             data_path: source data path
@@ -23,20 +22,19 @@ class DataLoad(object):
             dev_sample_rate: percentage of the training data to use for validation
         """
         self.data_path = data_path
-        self.mode = mode
         self.batch_size = batch_size
         self.num_epochs = num_epochs
         self.forced_seq_len = forced_seq_len
         self.dev_sample_rate = dev_sample_rate
         self._load_data()
 
-    def batch_iter(self, shuffle=True):
+    def train_batch_iter(self, shuffle=True):
         """
         params:
 
         returns:
         """
-        x, y, data_size = self._split_train_dev()
+        x, y, data_size = self._split_train_dev('train')
         num_batchs_per_epoch = data_size // self.batch_size + 1
 
         for _ in range(self.num_epochs):
@@ -47,6 +45,18 @@ class DataLoad(object):
                 start_idx = i * self.batch_size
                 end_idx = min((i+1) * self.batch_size, data_size)
                 yield x[start_idx:end_idx], y[start_idx:end_idx]
+
+    def get_dev_data(self, shuffle=True):
+        """
+        params:
+
+        returns:
+        """
+        dev_x, dev_y, dev_size = self._split_train_dev('dev')
+        if shuffle:
+            shuffled_indices = np.random.permutation(np.arange(dev_size))
+            dev_x, dev_y = dev_x[shuffled_indices], dev_y[shuffled_indices]
+        return dev_x, dev_y
 
     @staticmethod
     def _clean_str(s):
@@ -111,15 +121,15 @@ class DataLoad(object):
         self.x = np.array(x, dtype=np.int64)
         self.y = np.array(raw_y, dtype=np.float)
 
-    def _split_train_dev(self):
-        # split train set or dev set
+    def _split_train_dev(self, mode):
+        # split data into train set or dev set
         data_size = self.x.shape[0]
         dev_size = int(data_size * self.dev_sample_rate)
         train_size = data_size - dev_size
         # maybe using cross-validation is better
-        if self.mode == 'train':
+        if mode == 'train':
             return self.x[:train_size], self.y[:train_size], train_size
-        elif self.mode == 'dev':
+        elif mode == 'dev':
             return self.x[dev_size:], self.y[dev_size:], dev_size
         else:
             raise ValueError('mode shoudle be train or dev.')
@@ -151,12 +161,11 @@ class DataLoad(object):
 
 
 if __name__ == '__main__':
-    train_data = DataLoad(data_path=params.DATA_PATH,
-                          mode='train',
+    data = DataLoad(data_path=params.DATA_PATH,
                           batch_size=params.BATCH_SIZE,
                           num_epochs=params.NUM_EPOCHS,
                           forced_seq_len=params.FORCED_SEQ_LEN)
-    batches = train_data.batch_iter()
+    batches = data.train_batch_iter()
     batch_x, batch_y = next(batches)
     # print(len(batches))
     print(batch_x.shape)
--- text_clf/train.py
+++ text_clf/train.py
@@ -1,7 +1,8 @@
+import os
+import time
 import tensorflow as tf
 from data_load import DataLoad
 from model import TextCNN
-from tqdm import tqdm
 
 # Parameters
 # Data loading parameters
@@ -18,8 +19,8 @@ tf.app.flags.DEFINE_integer("embedding_size", 128,
                             "Dimensionality of character embedding (default: 128)")
 tf.app.flags.DEFINE_list("filters_size_list", [3, 4, 5],
                          "list type filter sizes (default: [3, 4, 5])")
-tf.app.flags.DEFINE_integer("num_filters", 4,
-                            "Number of filters per filter size (default: 128)")
+tf.app.flags.DEFINE_integer("num_filters", 8,
+                            "Number of filters per filter size (default: 8)")
 tf.app.flags.DEFINE_float("dropout_keep_prob", .5,
                           "Dropout keep probability (default: 0.5)")
 tf.app.flags.DEFINE_integer("num_classes", 39,
@@ -29,12 +30,8 @@ tf.app.flags.DEFINE_integer("num_classes", 39,
 tf.app.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
 tf.app.flags.DEFINE_integer("num_epochs", 200,
                             "Number of training epochs (default: 200)")
-# tf.app.flags.DEFINE_integer("evaluate_every", 100,
-#                             "Evaluate model on dev set after this many steps (default: 100)")
-# tf.app.flags.DEFINE_integer("checkpoint_every", 100,
-#                             "Save model after this many steps (default: 100)")
-# tf.app.flags.DEFINE_integer("num_checkpoints", 5,
-#                             "Number of checkpoints to store (default: 5)")
+tf.app.flags.DEFINE_integer("evaluate_every", 100,
+                            "Evaluate model on dev set after this many steps (default: 100)")
 
 # Misc Parameters
 tf.app.flags.DEFINE_boolean("allow_soft_placement", True,
@@ -51,43 +48,38 @@ with tf.Graph().as_default():
         log_device_placement=FLAGS.log_device_placement)
     sess = tf.Session(config=session_conf)
     with sess.as_default():
-        # loading training data 
-        train_set = DataLoad(data_path=FLAGS.train_data_path,
-                             mode='train',
-                             batch_size=FLAGS.batch_size,
-                             num_epochs=FLAGS.num_epochs,
-                             dev_sample_rate=FLAGS.dev_sample_rate)
-        train_batches = train_set.batch_iter()
+        # loading training data
+        data = DataLoad(data_path=FLAGS.train_data_path,
+                        batch_size=FLAGS.batch_size,
+                        num_epochs=FLAGS.num_epochs)
+        train_batches = data.train_batch_iter()
+
         # loading devolopment data
-        dev_set = DataLoad(data_path=FLAGS.train_data_path,
-                             mode='train',
-                             batch_size=FLAGS.batch_size,
-                             num_epochs=FLAGS.num_epochs,
-                             dev_sample_rate=FLAGS.dev_sample_rate)
-        dev_batches = dev_set.batch_iter()
-        
+        dev_x, dev_y = data.get_dev_data()
+
         # declare model
-        model = TextCNN(forced_seq_len=train_set.forced_seq_len,
-                        vocab_size=train_set.vocab_size,
+        model = TextCNN(forced_seq_len=data.forced_seq_len,
+                        vocab_size=data.vocab_size,
                         embedding_size=FLAGS.embedding_size,
                         filters_size_list=FLAGS.filters_size_list,
                         num_classes=FLAGS.num_classes,
                         num_filters=FLAGS.num_filters)
 
         # define training procedure
+        global_step = tf.Variable(0, name='global_step', trainable=False)
         optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate)
-        train_op = optimizer.minimize(model.loss_op)
+        train_op = optimizer.minimize(
+            loss=model.loss_op, global_step=global_step)
 
         # initialize all variables
         sess.run(tf.global_variables_initializer())
 
-        for step in range(FLAGS.num_epochs):
-            for _ in tqdm(range(1000)):
-                batch_x, batch_y = next(train_batches)
-                sess.run(train_op, feed_dict={model.inputs: batch_x, model.labels: batch_y,
-                                              model.dropout_keep_prob: FLAGS.dropout_keep_prob})
-            dev_x, dev_y = next(dev_batches)
-            loss, acc = sess.run([model.loss_op, model.accuracy_op],
-                                 feed_dict={model.inputs: dev_x, model.labels: dev_y,
-                                 model.dropout_keep_prob: FLAGS.dropout_keep_prob})
-            print('setps:{} | loss:{} | acc:{}'.format(step, loss, acc))
+        # training and eval into dev set
+        while 1:
+            batch_x, batch_y = next(train_batches)
+            _, step = sess.run([train_op, global_step], feed_dict={
+                               model.inputs: batch_x, model.labels: batch_y, model.dropout_keep_prob: FLAGS.dropout_keep_prob})
+            if step % FLAGS.evaluate_every == 0:
+                loss, acc = sess.run([model.loss_op, model.accuracy_op], feed_dict={
+                                     model.inputs: dev_x, model.labels: dev_y, model.dropout_keep_prob: FLAGS.dropout_keep_prob})
+                print('setp:{} | loss:{} | acc:{}'.format(step, loss, acc))
